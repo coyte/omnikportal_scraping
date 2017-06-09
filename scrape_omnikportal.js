27d49d48373e14166610b17f9f7fa59fb37b7dc6
coyte <niels@teekens.info>
2017-06-09 18:40:27 +0200

    Variable manip
    Getting the 5 values in a single var

--- scrape_omnikportal.js
+++ scrape_omnikportal.js
@@ -2,6 +2,7 @@
 var system = require('system');
 var args = system.args;
 var page = require('webpage').create();
+
 page.onConsoleMessage = function(msg) {
     console.log(msg);
 };
@@ -9,21 +10,21 @@ var url = args[1];
 page.open(url, function(status) {
     if (status === "success") {
             page.evaluate(function() {
-//                console.log(document.getElementsByClassName('chart_list_odd')[0].innerHTML); working but returns the whole 5 variables     
-                console.log(document.getElementById('ctl00_childPanel_lblNow').innerHTML);
-                console.log(document.getElementById('ctl00_childPanel_lblDEQ').innerHTML);
-                console.log(document.getElementById('ctl00_childPanel_lblMEQ').innerHTML);
-                console.log(document.getElementById('ctl00_childPanel_lblYEQ').innerHTML);
-                console.log(document.getElementById('ctl00_childPanel_lblSEQ').innerHTML);
+//                console.log(document.getElementsByClassName('chart_list_odd')[0].innerHTML); working but returns the whole 5 variables
+//                console.log(document.getElementById('ctl00_childPanel_lblNow').innerHTML);
+//                console.log(document.getElementById('ctl00_childPanel_lblDEQ').innerHTML);
+//                console.log(document.getElementById('ctl00_childPanel_lblMEQ').innerHTML);
+//                console.log(document.getElementById('ctl00_childPanel_lblYEQ').innerHTML);
+//                console.log(document.getElementById('ctl00_childPanel_lblSEQ').innerHTML);
+
+var tmp = document.getElementById('ctl00_childPanel_lblNow').innerHTML;
+console.log(tmp);
+var tmp = tmp.substring(0,tmp.indexOf(" ")) + ";";
+console.log(tmp);
 
         });
     } else {
-      console.log("not success");  
+      console.log("not success");
     }
-phantom.exit(0);
+phantom.kill(tmp);
 });
-
-
-
-
-
