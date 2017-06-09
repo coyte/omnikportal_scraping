699aa1f655858122ad1a6cd7af26306e5de37c6c
coyte <niels@teekens.info>
2017-06-09 16:57:03 +0200

    initial

---
+++ readme.md
@@ -0,0 +1,6 @@
+#scrape_omnikportal.js
+
+This script file is intended to be run with phantomJS:
+phantomjs scrape_omnikportal.js <url you use to view your systems>
+
+It logs the values from the portal
---
+++ scrape_omnikportal.js
@@ -0,0 +1,29 @@
+"use strict";
+var system = require('system');
+var args = system.args;
+var page = require('webpage').create();
+page.onConsoleMessage = function(msg) {
+    console.log(msg);
+};
+var url = args[1];
+page.open(url, function(status) {
+    if (status === "success") {
+            page.evaluate(function() {
+//                console.log(document.getElementsByClassName('chart_list_odd')[0].innerHTML); working but returns the whole 5 variables     
+                console.log(document.getElementById('ctl00_childPanel_lblNow').innerHTML);
+                console.log(document.getElementById('ctl00_childPanel_lblDEQ').innerHTML);
+                console.log(document.getElementById('ctl00_childPanel_lblMEQ').innerHTML);
+                console.log(document.getElementById('ctl00_childPanel_lblYEQ').innerHTML);
+                console.log(document.getElementById('ctl00_childPanel_lblSEQ').innerHTML);
+
+        });
+    } else {
+      console.log("not success");  
+    }
+phantom.exit(0);
+});
+
+
+
+
+
