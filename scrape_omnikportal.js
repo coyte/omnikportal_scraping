87e4d2cfc10cca139b64ca7065857fe95e2faa72
coyte <niels@teekens.info>
2017-06-11 12:56:54 +0200

    Updates

--- scrape_omnikportal.js
+++ scrape_omnikportal.js
@@ -10,21 +10,22 @@ var url = args[1];
 page.open(url, function(status) {
     if (status === "success") {
             page.evaluate(function() {
-//                console.log(document.getElementsByClassName('chart_list_odd')[0].innerHTML); working but returns the whole 5 variables
-//                console.log(document.getElementById('ctl00_childPanel_lblNow').innerHTML);
-//                console.log(document.getElementById('ctl00_childPanel_lblDEQ').innerHTML);
-//                console.log(document.getElementById('ctl00_childPanel_lblMEQ').innerHTML);
-//                console.log(document.getElementById('ctl00_childPanel_lblYEQ').innerHTML);
-//                console.log(document.getElementById('ctl00_childPanel_lblSEQ').innerHTML);
+                var lblNow = document.getElementById('ctl00_childPanel_lblNow').innerHTML;
+                var lblNow = lblNow.substring(0,lblNow.indexOf(" "));
+                var lblDEQ = document.getElementById('ctl00_childPanel_lblDEQ').innerHTML;
+                var lblDEQ = lblDEQ.substring(0,lblDEQ.indexOf(" "));
+                var lblMEQ = document.getElementById('ctl00_childPanel_lblMEQ').innerHTML;
+                var lblMEQ = lblMEQ.substring(0,lblMEQ.indexOf(" "));
+                var lblYEQ = document.getElementById('ctl00_childPanel_lblYEQ').innerHTML;
+                var lblYEQ = lblYEQ.substring(0,lblYEQ.indexOf(" "));
+                var lblSEQ = document.getElementById('ctl00_childPanel_lblSEQ').innerHTML;
+                var lblSEQ = lblSEQ.substring(0,lblSEQ.indexOf(" "));
 
-var tmp = document.getElementById('ctl00_childPanel_lblNow').innerHTML;
-console.log(tmp);
-var tmp = tmp.substring(0,tmp.indexOf(" ")) + ";";
-console.log(tmp);
-
+                var omnikData = lblNow + ";" + lblDEQ + ";" + lblMEQ + ";" + lblYEQ + ";" + lblSEQ;
+                console.log(omnikData);
         });
     } else {
       console.log("not success");
     }
-phantom.kill(tmp);
+phantom.exit();
 });
